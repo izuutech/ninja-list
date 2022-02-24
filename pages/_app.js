42bd367a3b8db7c7d5242ffbfe41244e6a89eb67
unknown <amaechiizu@gmail.com>
2022-02-24 04:24:35 +0100

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Layout from '../comps/Layout'
+import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+
+
+const queryClient= new QueryClient();
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+    <QueryClientProvider client={queryClient}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>
+                    About
+                </h1>
+                <p>Excepteur do nisi est ipsum nulla est commodo mollit incididunt do commodo ut. Excepteur do nisi est ipsum nulla est commodo mollit incididunt do commodo ut.</p>
+                <p>Excepteur do nisi est ipsum nulla est commodo mollit incididunt do commodo ut. Excepteur do nisi est ipsum nulla est commodo mollit incididunt do commodo ut.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
